7a9f946ed1983cef698de3743864c438b0143851
Lucky Acidera <lucky.a.acidera@gmail.com>
2026-04-13 01:31:42 +0800

    feat: add domain entities

--- server/Kliniq/src/Kliniq.Domain/Entities/Patient.cs
+++ server/Kliniq/src/Kliniq.Domain/Entities/Patient.cs
@@ -1,14 +1,69 @@
 ﻿using Kliniq.Domain.Common;
+using Kliniq.Domain.Enums;
+using Kliniq.Domain.ValueObjects;
 
 namespace Kliniq.Domain.Entities
 {
     public class Patient : AuditableEntity
     {
         public Guid UserId { get; private set; }
-        
-        public string FirstName { get; private set; } = string.Empty;   
+       
+        public FullName Name { get; private set; }
+        public DateTime DateOfBirth { get; private set; }
+        public Gender Gender { get; private set; }
+        public Address Address { get; private set; }
 
+        public string? PhoneNumber { get; private set; }
+        public string? EmergencyContact { get; private set; }
 
         private Patient() { }
+
+        public Patient(
+            Guid userId,
+            FullName name,
+            DateTime dateOfBirth,
+            Gender gender,
+            Address address,
+            string? phoneNumber,
+            string? emergencyContact
+            )            
+        {
+            if (dateOfBirth > DateTime.UtcNow)
+                throw new ArgumentException("Date of birth cannot be in the future");
+            
+            Id = Guid.NewGuid();
+            UserId = userId;
+            Name = name;
+            DateOfBirth = dateOfBirth;
+            Gender = gender;
+            Address = address;
+            PhoneNumber = phoneNumber;
+            EmergencyContact = emergencyContact;
+        }
+
+
+        public int Age => DateTime.UtcNow.Year - DateOfBirth.Year;
+
+        public void UpdateAddress(Address newAddress)
+        {
+            Address = newAddress;
+            UpdatedAtUtc = DateTime.UtcNow;
+        }
+        public void UpdatePhoneNumber(string newPhoneNumber)
+        {
+            PhoneNumber = newPhoneNumber;
+            UpdatedAtUtc = DateTime.UtcNow;
+        }
+        public void UpdateEmergencyContact(string newEmergencyContact)
+            {
+                EmergencyContact = newEmergencyContact;
+                UpdatedAtUtc = DateTime.UtcNow;
+        }
+        public void UpdateName(FullName newName)
+        {
+            Name = newName;
+            UpdatedAtUtc = DateTime.UtcNow;
+        }
+
     }
 }
--- server/Kliniq/src/Kliniq.Domain/ValueObjects/Address.cs
+++ server/Kliniq/src/Kliniq.Domain/ValueObjects/Address.cs
@@ -1,10 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Kliniq.Domain.Common;
 
 namespace Kliniq.Domain.ValueObjects
 {
-    internal class Address
+    public class Address : ValueObject
     {
+        public string Street { get; } = null!;
+        public string City { get; } = null!;
+        public string Country { get; } = null!;
+
+
+        private Address() { }
+
+        public Address(string street, string city, string country)
+        {
+            Street = string.IsNullOrWhiteSpace(street)
+                ? throw new ArgumentException("Street is required")
+                : street;
+
+            City = string.IsNullOrWhiteSpace(city)
+                ? throw new ArgumentException("City is required")
+                : city;
+
+            Country = string.IsNullOrWhiteSpace(country)
+                ? throw new ArgumentException("Country is required")
+                : country;  
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Street;
+            yield return City;
+            yield return Country;
+        }
     }
 }
